8e712e3585b602fa27d30c9bba96309a13156bdf
shubhamjirge <141141304+shubhamjirge@users.noreply.github.com>
2025-01-23 11:46:11 +0530

    Add files via upload

---
+++ Mathematical_Operations.py
@@ -0,0 +1,20 @@
+# MATHEMATICAL OPERATIONS
+# PEMDAS
+# Parentheses, Exponents, Multiplication/Division, Addition/Subtraction
+
+print("My age: " + str(12))
+print(12+50)
+print(type(12+50))
+print(type(20-10))
+print(type(20*10))
+print(type(20/4))
+print(20/4)
+print(2**3)
+
+
+
+print(3 * 3 + 3 / 3 - 3)
+print(3 + 3 * 3 / 3 - 3)
+print(1.65*2)
+print(6 + 4 / 2 - (1 * 2))
+
